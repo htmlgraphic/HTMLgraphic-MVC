b7deef4dc084148054031e739fe6fdbf6b34a4b8
Tom Farvour <tom@farvour.com>
2011-03-08 17:28:10 -0600

    user all controller for listing the data table

---
+++ controller/com/passinggreen/www/procedure/AdminUserGetAllController.class.inc.php
@@ -0,0 +1,68 @@
+<?php
+Loader::load("controller", "/controller/ModelController");
+
+class AdminUserGetAllController extends ModelController
+{
+	function __construct()
+	{
+
+	}
+
+	function execute()
+	{
+		$return = new stdClass;
+		$params = Request::getRequest();
+
+		Config::set("HideDebugger", true); //comment this out to debug
+
+		$users = DBObject::collection('Member', DBObject::CONSISTENCY_ABSOLUTE);
+		$fullCount = $users->getMembersCount();
+
+		$users = DBObject::collection('Member', DBObject::CONSISTENCY_ABSOLUTE);
+		$users->setRange($params['iDisplayStart'], $params['iDisplayLength']);
+
+		switch ($params['iSortCol_0'])
+		{
+			case 0:
+				$users->applySort("AutoID", $params['sSortDir_0']);
+				break;
+			case 1:
+				$users->applySort("userFirstname", $params['sSortDir_0']);
+				break;
+			case 2:
+				$users->applySort("userLastname", $params['sSortDir_0']);
+				break;
+			case 3:
+				$users->applySort("useremail", $params['sSortDir_0']);
+				break;
+		}
+
+		$return = array(
+		    'sEcho' => $params['sEcho'],
+		    'iTotalRecords' => $fullCount,
+		    'iTotalDisplayRecords' => $params['iDisplayLength'],
+		    'aaData' => array()
+		);
+
+		foreach ($users->getMembers() as $_user)
+		{
+			$row = array();
+
+			$row[] = $_user->getID();
+			$row[] = $_user->getFirstname();
+			$row[] = $_user->getLastname();
+			$row[] = $_user->getEmail();
+			$row[] = $_user->getLevel();
+			$row[] = $_user->getIsEnabled();
+			$row[] = $_user->getLastLogin();
+			$row[] = "<a onclick=\"editUser('" . $_user->getID() . "');\"><span class=\"ui-icon ui-icon-wrench\"></span></a><a onclick=\"deleteUser('" . $_user->getID() . "');\"><span class=\"ui-icon ui-icon-trash\"></span></a>";
+
+			$return['aaData'][] = $row;
+		}
+
+		echo json_encode($return);
+		return;
+	}
+
+}
+?>
