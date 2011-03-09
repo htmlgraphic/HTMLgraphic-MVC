3c759ca75e14c87712c363e978c483dbd63f17f4
Tom Farvour <tom@farvour.com>
2011-03-08 20:22:12 -0600

    mysql db pass changes

--- utility/database/mysql/settings/com/passinggreen/cfg_write.php
+++ utility/database/mysql/settings/com/passinggreen/cfg_write.php
@@ -1,7 +1,7 @@
 <?php
 $settings['host'] = 'localhost';
 $settings['username'] = 'passinggreen';
-$settings['passwd'] = 'XtB4e5UNtJJyiiHpZLPL1hWH';
+$settings['passwd'] = 'referhinmone';
 $settings['dbname'] = 'passinggreen_com';
 $settings['port'] = 3306;
 $settings['timeout'] = 3;
