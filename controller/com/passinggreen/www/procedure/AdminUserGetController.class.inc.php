3c7a67bc9635016869722658e2e41421d8419e18
Tom Farvour <tom@farvour.com>
2011-03-14 11:14:44 -0500

    two way file rename

--- controller/com/passinggreen/www/procedure/AdminUserGetController.class.inc.php
+++ controller/com/passinggreen/www/procedure/AdminUserGetController.class.inc.php
@@ -31,25 +31,33 @@ class AdminUserGetController extends ModelController {
                 Debugger::log("refs passed: $user_referrals_passed");
                 Debugger::log("refs recvd: $user_referrals_received");
 
-                $user_crypt_key = CryptKey::findCryptKeyByUserID($user->getID());
-
                 // decrypt cc field if it is not empty
+                $user_crypt_key = CryptKey::findCryptKeyByUserID($user->getID());
                 if (isset($user_crypt_key) && $user_crypt_key->isValid()) {
+                    Debugger::log("User crypt private key: " . $user_crypt_key->getKey());
                     $decoded_cc_data = TwoWayEncryption::decrypt($user->getCC(), $user_crypt_key->getKey());
+                } else {
+                    $decoded_cc_data = null;
                 }
 
                 $return->id = $user->getID(); // object id
                 $return->user = $user->toArray(); // set the base return, values can be overridden below.
-                $return->user['userCompanyType[]'] = unserialize($return->user['userCompanyType']);
-                $return->user['siteAreas[]'] = explode(',', $return->user['siteAreas']);
-                $return->user['balance'] = number_format($user_balance, 2);
-                $return->user['ccc'] = $decoded_cc_data;
-                $return->user['ccc_status'] = $decoded_cc_data['status'];
-                $return->user['ccc_tcode'] = $decoded_cc_data['tcode'];
-                $return->user['ccc_ccNum'] = $decoded_cc_data['ccNum'];
-                $return->user['ccc_MM'] = $decoded_cc_data['ccMM'];
-                $return->user['ccc_YY'] = $decoded_cc_data['ccYear'];
-                $return->user['ccc_ccCode'] = $decoded_cc_data['ccCODE'];
+                $return->user["userCompanyType[]"] = unserialize($return->user['userCompanyType']);
+                $return->user["siteAreas[]"] = explode(',', $return->user['siteAreas']);
+                $return->user["balance"] = number_format($user_balance, 2);
+
+                if (!is_null($decoded_cc_data)) {
+                    $return->user["ccc"] = $decoded_cc_data;
+                    $return->user["ccc_status"] = $decoded_cc_data['status'];
+                    $return->user["ccc_tcode"] = $decoded_cc_data['tcode'];
+                    $return->user["ccc_ccNum"] = $decoded_cc_data['ccNum'];
+                    $return->user["ccc_MM"] = $decoded_cc_data['ccMM'];
+                    $return->user["ccc_YY"] = $decoded_cc_data['ccYear'];
+                    $return->user["ccc_ccCode"] = $decoded_cc_data['ccCODE'];
+                }
+
+                $return->user["referralsPassed"] = $user_referrals_passed;
+                $return->user["referralsReceived"] = $user_referrals_received;
 
                 echo json_encode($return);
                 return;
